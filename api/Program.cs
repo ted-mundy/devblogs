02d2dae2553c809034e2bd805a4bfa01d4b4df2b
Ted Mundy <ted@mundy.io>
2024-01-04 08:59:34 +0000

    fix(api): remove sln

--- api/Program.cs
+++ api/Program.cs
@@ -7,7 +7,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World Bozo!");
+        app.MapGet("/", () => "Hello World!");
 
         app.Run();
     }
